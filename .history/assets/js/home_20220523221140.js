806651e8301e4e45510bd9158943fd8ff3d34ec1
Đào Đức Hiếu <77675580+DaoDucHieu1712@users.noreply.github.com>
2022-06-02 20:59:56 +0700

    delete history
    delete successful !

--- .history/assets/js/home_20220523221140.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-    $('.home-course__list').slick({
-    });
-});
--- .history/assets/js/home_20220524003033.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $(".home-course__list").slick({
-        slidesToShow: 5,
-        slidesToScroll: 2
-    });
-});
