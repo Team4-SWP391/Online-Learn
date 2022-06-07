f90b20f75351e2fd595d86f5e06b237c510d8148
linhntmhe151096 <100248364+linhntmhe151096@users.noreply.github.com>
2022-06-07 22:36:47 +0700

    fix courseDetails

---
+++ assets/js/courseDetail.js
@@ -0,0 +1,17 @@
+let pointScroll = 'default'
+document.onscroll = () => {
+    // console.log(test.offsetTop)
+    console.log(window.scrollY)
+    const scrollValue = window.scrollY
+
+    if(scrollValue >= 750) {
+        pointScroll = 'modify'
+        console.log(pointScroll)
+        test.classList.add('modify')
+    }else {
+        pointScroll = 'default'
+        console.log(pointScroll)
+        test.classList.remove('modify')
+
+    }
+}
