9116db394836b0a6f21d9faa502fe79ecb2d0272
Vinhlam2512 <lamvinh2431@gmail.con>
2022-06-23 23:04:28 +0700

    a

--- SWP_TEAM_4/SWP_TEAM_4/Online_Learn/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Online_Learn.Models {
-    public class ErrorViewModel {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
