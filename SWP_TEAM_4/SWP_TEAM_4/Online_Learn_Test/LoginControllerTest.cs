3d95659f31ce63c02dabeb9da3fdaf092f915757
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2022-06-29 22:48:10 +0700

    add project test

---
+++ SWP_TEAM_4/SWP_TEAM_4/Online_Learn_Test/LoginControllerTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+
+using NUnit.Framework;
+
+using Online_Learn.Controllers;
+using Online_Learn.Models;
+
+namespace Online_Learn_Test {
+    [TestFixture]
+    internal class LoginControllerTest {
+        protected Online_LearnContext context;
+        protected LoginController loginController;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<Online_LearnContext>()
+            .UseInMemoryDatabase(databaseName: "Online_Learn")
+            .Options;
+            context = new Online_LearnContext(options);
+        }
+
+
+        [Test]
+        public void TestLogin()
+        {
+            loginController = new LoginController(context);
+            var result = loginController.GetAccount("admin@gmail.com", "123");
+            Assert.AreEqual(result, null);
+        }
+    }
+}
