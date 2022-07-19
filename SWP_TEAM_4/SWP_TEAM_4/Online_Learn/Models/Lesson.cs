180bdfbb08eca51a27d21451e845a73ee6123420
linhntmhe151096 <100248364+linhntmhe151096@users.noreply.github.com>
2022-07-19 18:37:47 +0700

    edit lesson

--- SWP_TEAM_4/SWP_TEAM_4/Online_Learn/Models/Lesson.cs
+++ SWP_TEAM_4/SWP_TEAM_4/Online_Learn/Models/Lesson.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -13,7 +14,9 @@ namespace Online_Learn.Models
         }
 
         public int LessonId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Lesson Name is requried...")]
         public string LessonName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Video is requỉed...")]
         public string Video { get; set; }
         public string Main { get; set; }
         public int? LectureId { get; set; }
